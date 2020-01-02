fce44e74627f1e394e970b66e0f094f2a7225c34
Michael Benham <michaelbenham89@gmail.com>
2020-01-02 18:09:24 -0500

    Updates

---
+++ precache-manifest.62fd1af5b9c4846dd9f2bd35f6ff21ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a0cb74500087119a1c6330db4b79d44",
+    "url": "/react-amplify/index.html"
+  },
+  {
+    "revision": "5079096c3cc042a32132",
+    "url": "/react-amplify/static/css/2.65b39531.chunk.css"
+  },
+  {
+    "revision": "79723125439763ff2de5",
+    "url": "/react-amplify/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5079096c3cc042a32132",
+    "url": "/react-amplify/static/js/2.44cdee46.chunk.js"
+  },
+  {
+    "revision": "0243f6fd1e8a300c53eafce5405c216e",
+    "url": "/react-amplify/static/js/2.44cdee46.chunk.js.LICENSE"
+  },
+  {
+    "revision": "79723125439763ff2de5",
+    "url": "/react-amplify/static/js/main.3d441bcc.chunk.js"
+  },
+  {
+    "revision": "1984ea26794337f57000",
+    "url": "/react-amplify/static/js/runtime-main.42c10ac3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-amplify/static/media/logo.5d5d9eef.svg"
+  }
+]);
